ed1f56435053fe9cc14747aa7382a983f084fdba
Manuelma4 <mmayaro@unal.edu.co>
2023-03-12 22:04:07 -0500

    Se agregaron librerias y se agregaron comentarios

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -52,25 +52,26 @@ const userController = {
     } catch (error) {
       res.status(500).json({ error: 'Errorupdate' });
     }
-  },
-
-  async delete(req: Request, res: Response) {
-    try {
-      const { id } = req.params;
-      const user = await User.findById(id);
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
-      if (!('_id' in user)) {
-        throw new Error('Invalid user object');
-      }
-      await user.delete();
-      res.status(200).json({ message: 'User deleted successfully' });
-    } catch (error) {
-      console.error(error); // registrar el error en la consola
-      res.status(500).json({ error: 'Error deleting user' });
-    }
   }
+  //,
+
+  // async delete(req: Request, res: Response) {
+  //   try {
+  //     const { id } = req.params;
+  //     const user = await User.findById(id);
+  //     if (!user) {
+  //       return res.status(404).json({ error: 'User not found' });
+  //     }
+  //     if (!('_id' in user)) {
+  //       throw new Error('Invalid user object');
+  //     }
+  //     await user.delete();
+  //     res.status(200).json({ message: 'User deleted successfully' });
+  //   } catch (error) {
+  //     console.error(error); // registrar el error en la consola
+  //     res.status(500).json({ error: 'Error deleting user' });
+  //   }
+  // }
 };
 
 export default userController;
